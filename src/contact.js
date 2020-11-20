94de625d8b0926a37fb047c81246b8ce86bb8453
oloomoses <olooine@gmail.com>
2020-11-20 21:06:02 +0300

    css style for the contact form

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,15 @@
 function form(){
   const form = document.createElement('form');
   const child = `
-  <label>Enter your name</label>
-  <input type = 'text' class = 'f-name' /><br>
-  <label>Email</label>
-  <input type = 'email' class = 'f-email' /><br>
+  <label class = 'label'>Enter your name</label><br>
+  <input type = 'text' class = 'f-name f' /><br>
+  <label class = 'label'>Email</label><br>
+  <input type = 'email' class = 'f-email f' /><br>
 
-  <label>Message</label>
+  <label class = 'label'>Message</label><br>
   <textarea class = 'textarea'></textarea>
+
+  <button type = 'submit' class = 'submit'>Submit</submit>
   `
 
   form.innerHTML = child;
@@ -19,6 +21,12 @@ export const contact = () => {
   const div = document.createElement('div');
   div.classList.add('form-area');
   div.appendChild(form());
+
+  const h2 = document.createElement('h2');
+  h2.classList.add('sub-header');
+  h2.innerHTML = 'Leave a Message';
+
+  content.appendChild(h2);
 
   content.appendChild(div);
   return content;
